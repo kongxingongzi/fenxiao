4e5b2e0b9bdee7df2ee2fb7602580fd144cfaa84
kongxingongzi <1543892875@qq.com>
2018-07-21 02:38:49 +0800

    dddd

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php 
+require __DIR__.'/vendor/autoload.php';
+
+use EasyWeChat\Factory;
+
+$options = [
+    'app_id'    => 'wx54f0e94fbee136c0',
+    'secret'    => '6ec9e287b58a6a06ef8cb04c604e9113',
+    'token'     => 'token',
+    // 'log' => [
+    //     'level' => 'debug',
+    //     'file'  => '/tmp/easywechat.log',
+    // ],
+    // ...
+];
+
+$app = Factory::officialAccount($options);
+
+$server = $app->server;
+$user = $app->user;
+
+$server->push(function($message) use ($user) {
+    $fromUser = $user->get($message['FromUserName']);
+
+    return "{$fromUser->nickname} 您好！欢迎关注 overtrue!";
+});
+
+$server->serve()->send();
